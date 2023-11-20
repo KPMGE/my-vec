f6f32402b5ef65e2974f6ca076dfc259a13a5f16
KPMGE <jesuskevin254@gmail.com>
2023-11-20 06:03:50 -0300

    :white_check_mark: ensure macro creates a vector with multiple elements

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,29 @@
+// NOTE: we use the $() for repetition, basically, anytime we 
+// match the expression, we're gonna execute it on the expression block
+
 #[macro_export]
 macro_rules! my_vec {
     () => {
         Vec::new()
-    }
+    };
+    ($($element: expr),+) => {{
+        let mut v = Vec::new();
+        $(v.push($element);)+
+        v
+    }};
 }
 
 #[test]
 fn should_make_an_empty_vec() {
     let x: Vec<u32> = my_vec![];
     assert!(x.is_empty());
+}
+
+#[test]
+fn should_make_a_vec_with_comma_separated_elements() {
+    let x: Vec<u32> = my_vec![1, 2, 3];
+    assert!(!x.is_empty());
+    assert_eq!(x[0], 1);
+    assert_eq!(x[1], 2);
+    assert_eq!(x[2], 3);
 }
